cef8d39179b0891c91fc9cd3555de5356c0ba636
Daniel <dalmodovar.1337@gmail.com>
2015-10-29 17:25:45 -0400

    Beautified (re-indented) js files
    Used js-beautifier + a few tweaks, like indented this.emit() arguments

--- handlers/helper.js
+++ handlers/helper.js
@@ -1,29 +1,29 @@
 var Dota2 = require("../index"),
     util = require("util");
-    
+
 // Helper methods
 Dota2._parseOptions = function(options, possibleOptions) {
-  var details, option, type, value;
+    var details, option, type, value;
 
-  details = {};
+    details = {};
 
-  for (option in options) {
-    value = options[option];
-    type = possibleOptions[option];
-    if (type == null) {
-      if (this.debug) {
-        util.log("Option " + option + " is not possible.");
-      }
-      continue;
+    for (option in options) {
+        value = options[option];
+        type = possibleOptions[option];
+        if (type == null) {
+            if (this.debug) {
+                util.log("Option " + option + " is not possible.");
+            }
+            continue;
+        }
+        if (typeof value !== type) {
+            if (this.debug) {
+                util.log("Option " + option + " must be a " + type + ".");
+            }
+            continue;
+        }
+        details[option] = value;
     }
-    if (typeof value !== type) {
-      if (this.debug) {
-        util.log("Option " + option + " must be a " + type + ".");
-      }
-      continue;
-    }
-    details[option] = value;
-  }
 
-  return details;
+    return details;
 }
