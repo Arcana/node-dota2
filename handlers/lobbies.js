371ef4c77d5b73e3d3ca80492e4eac5510ae1788
dhdeubot <dhdeubot@gmail.com>
2015-12-08 12:08:31 +0000

    Refactored lobbies to remove duplicate code (cont'd)

--- handlers/lobbies.js
+++ handlers/lobbies.js
@@ -242,27 +242,18 @@ Dota2.Dota2Client.prototype.addBotToPracticeLobby = function(slot, team, bot_dif
     slot = slot || 1;
     team = team || Dota2.schema.DOTA_GC_TEAM.DOTA_GC_TEAM_GOOD_GUYS;
     bot_difficulty = bot_difficulty || Dota2.schema.DOTABotDifficulty.BOT_DIFFICULTY_PASSIVE;
-    
     var _self = this;
-    if (!this._gcReady) {
-        if (this.debug) util.log("GC not ready, please listen for the 'ready' event.");
-        return null;
-    }
 
     if (this.debug) util.log("Sending match CMsgPracticeLobbySetTeamSlot request");
+    
     var payload = new Dota2.schema.CMsgPracticeLobbySetTeamSlot({
         "team": team,
         "slot": slot,
         "bot_difficulty": bot_difficulty
     });
-    this._protoBufHeader.msg = Dota2.schema.EDOTAGCMsg.k_EMsgGCPracticeLobbySetTeamSlot;
-    this._gc.send(
-        this._protoBufHeader,
-        payload.toBuffer(),
-        function(header, body) {
-            onPracticeLobbyResponse.call(_self, body, callback);
-        }
-    );
+    this.sendToGC(  Dota2.schema.EDOTAGCMsg.k_EMsgGCPracticeLobbySetTeamSlot, 
+                    payload, 
+                    Dota2._convertCallback.call(_self, onPracticeLobbyResponse, callback));
 }
 
 // Handlers
