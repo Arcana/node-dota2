6309d4a8e7f00de143f982b4cb4bc7ad36f57f4e
dhdeubot <dhdeubot@gmail.com>
2016-12-02 14:56:40 +0000

    Added support for fetching player cards

--- handlers/fantasy.js
+++ handlers/fantasy.js
@@ -1,7 +1,68 @@
 var Dota2 = require("../index"),
-    util = require("util");
+    util = require("util"),
+    Long = require('long');
 
 // Methods
+
+// Returns a list of promises which resolve to a list of players, with player stats if the call to the GC was succesful.
+// Maximum delay before promises resolve is ~20s. 
+// A player object looks like this:
+// {
+//     'account_id' : uint32,
+//     'cards' : [{'id' : uint32, 'bonuses' : uint64}],
+//     'stats' : CMsgGCToClientPlayerStatsResponse (if GC responded before 2s timeout)
+// }
+// For magic numbers, see -> https://raw.githubusercontent.com/SteamDatabase/GameTracking/master/dota/game/dota/pak01_dir/scripts/items/items_game.txt
+Dota2.Dota2Client.prototype.requestPlayerCardsByPlayer = function() {
+    if(this.Inventory) {
+        var playercards = this.Inventory.filter(item => item.def_index == 11953);
+        var promises = [];
+        // Sort cards per player
+        var players = playercards.reduce((players, card)=>{
+            var id_attr = card.attribute.filter(attr => attr.def_index == 424)[0];
+            var account_id = Buffer.from(id_attr.value_bytes.buffer).readUInt32LE(id_attr.value_bytes.offset);
+            // Add player if we haven't seen him yet
+            if (!players[account_id]) players[account_id] = {'account_id': account_id, 'cards': []};
+            // Add this card
+            var bonus = card.attribute.filter(attr => attr.def_index == 425)[0];
+            players[account_id].cards.push({
+                'id': card.id,
+                'bonuses': bonus ?  new Long(Buffer.from(bonus.value_bytes.buffer).readUInt32LE(bonus.value_bytes.offset + 4),
+                                    Buffer.from(bonus.value_bytes.buffer).readUInt32LE(bonus.value_bytes.offset), 
+                                    true)
+                                :   undefined
+            });
+            return players;
+        },{});
+        // Send a player stats request for each player account id
+        Object.keys(players).forEach((player, i)=>{
+            promises.push(
+                new Promise((resolve, reject) => {
+                    // Stagger the requests with 200ms intervals
+                    setTimeout(() =>{
+                        this.requestPlayerStats(players[player].account_id, (err, result) => {
+                            if (err) return reject(err);
+                            return resolve(
+                                (() => {
+                                    players[player].stats = result;
+                                    return players[player];
+                                })()
+                            );
+                        });
+                        // Resolve if we haven't received a response after 2s
+                        setTimeout(()=>resolve(players[player]), 2000);
+                    }, i * 200);
+                })
+                
+            );
+        });
+        
+        return promises;
+    } else {
+        return null;
+    }
+}
+
 // // This proto message is not supported by Valve's backend as of yet
 // Dota2.Dota2Client.prototype.requestPlayerCardItemInfo = function(player_card_ids, callback) {
 //     player_card_ids = player_card_ids.constructor == Array ? player_card_ids : [player_card_ids];
