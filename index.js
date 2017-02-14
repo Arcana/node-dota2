34935f96bd850e3fe72e34df1e42e88c5ac32a18
dhdeubot <dhdeubot@gmail.com>
2017-02-14 13:49:00 +0000

    Small fix for inventory

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ Dota2.Dota2Client = function Dota2Client(steamClient, debug, debugMore) {
     this.debugMore = debugMore || false;
     
     /** The current state of the bot's inventory. Contains cosmetics, player cards, ... 
-    * @type {CSOEconItem []} 
+    * @type {CSOEconItem[]} 
     */
     this.Inventory = [];
     /** The chat channels the bot has joined */
