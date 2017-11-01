5a25d2dd5cfaa7c9e03b999151c0af2e8778910f
dhdeubot <dhdeubot@gmail.com>
2017-11-01 16:27:44 +0000

    Add destroy lobby, remove deprecated stuff, update dependencies

--- handlers/leagues.js
+++ handlers/leagues.js
@@ -2,37 +2,6 @@ var Dota2 = require("../index"),
     util = require("util");
 
 // Methods
-/**
- * Sends a message to the Game Coordinator requesting data on leagues being played in the given month.  
- * Provide a callback or listen for {@link module:Dota2.Dota2Client#event:leaguesInMonthData|leaguesInMonthData} for the Game Coordinator's response. 
- * Requires the GC to be {@link module:Dota2.Dota2Client#event:ready|ready}.
- * @alias module:Dota2.Dota2Client#requestLeaguesInMonth
- * @param {number} [month=(new Date()).getMonth()] - Month (MM) you want to query data for. **IMPORTANT NOTE**:  Month is zero-aligned, not one-aligned; so Jan = 00, Feb = 01, etc.
- * @param {number} [year=(new Date()).getFullYear()] - Year (YYYY) you want to query data for.
- * @param {number} [tier=0] - Search only for a specific tier of tournaments. 
- * @param {module:Dota2~requestCallback} [callback] - Called with `err, CMsgDOTALeaguesInMonthResponse`
- */
-Dota2.Dota2Client.prototype.requestLeaguesInMonth = function(month, year, tier, callback) {
-    var _self = this;
-    var now = new Date();
-
-    // Month & year default to current time values
-    month = month === undefined ? now.getMonth() : month;
-    year = year || now.getFullYear();
-    tier = tier || 0;
-    callback = callback || null;
-
-    this.Logger.debug("Sending CMsgDOTALeaguesInMonthRequest");
-    
-    var payload = {
-        'month': month,
-        'year': year,
-        'tier': tier
-    };
-    this.sendToGC(  Dota2.schema.lookupEnum("EDOTAGCMsg").values.k_EMsgGCLeaguesInMonthRequest, 
-                    Dota2.schema.lookupType("CMsgDOTALeaguesInMonthRequest").encode(payload).finish(), 
-                    onLeaguesInMonthResponse, callback);
-};
 
 /**
  * Requests info on all available official leagues from the GC.
@@ -68,25 +37,6 @@ Dota2.Dota2Client.prototype.requestTopLeagueMatches = function() {
 
 // Events
 /**
- * Emitted in response to a {@link module:Dota2.Dota2Client#requestLeaguesInMonth|request for the leagues in a given month}.
- * The leagues that are returned are those which have one or more matches in the given month.
- * They can however also have other matches in a different month.
- * @event module:Dota2.Dota2Client#leaguesInMonthData
- * @param {number} month - Month this data represents.
- * @param {number} year - Year this data represents
- * @param {Object[]} leagues - List of leagues that have matches in the given month
- * @param {number} leagues[].league_id - ID of the league
- * @param {Object[]} leagues[].schedule - The scheduled games in this league. Might contain matches in other months.
- * @param {number} leagues[].schedule[].block_id - ID of the schedule block
- * @param {number} leagues[].schedule[].start_time - Unix timestamp of the start time of this scheduled match
- * @param {boolean} leagues[].schedule[].finals - Whether or not this is a finals game
- * @param {string} leagues[].schedule[].comment - Comment about this scheduled block; often the team names & position in bracket
- * @param {Object[]} leagues[].schedule[].teams - The teams duking it out in this match
- * @param {number} leagues[].schedule[].teams[].team_id - ID of the team. Not every participating team seems to be hooked up to Dota 2's team system, so 0 entries can happen
- * @param {string} leagues[].schedule[].teams[].name - Name of the team
- * @param {external:Long} leagues[].schedule[].teams[].logo - Logo of the team
- */
-/**
  * Emitted when the GC sends a `CMsgDOTALiveLeagueGameUpdate`.
  * @event module:Dota2.Dota2Client#liveLeagueGamesUpdate
  * @param {number} live_league_games - The number of live league games
@@ -118,21 +68,6 @@ Dota2.Dota2Client.prototype.requestTopLeagueMatches = function() {
  
 // Handlers
 var handlers = Dota2.Dota2Client.prototype._handlers;
-
-var onLeaguesInMonthResponse = function onLeaguesInMonthResponse(message, callback) {
-    callback = callback || null;
-    var response = Dota2.schema.lookupType("CMsgDOTALeaguesInMonthResponse").decode(message);
-
-    if (response.eresult === 1) {
-        this.Logger.debug("Received leagues in month response " + response.eresult);
-        this.emit("leaguesInMonthData", response.month, response.year, response.leagues);
-        if (callback) callback(null, response);
-    } else {
-        this.Logger.error("Received a bad leaguesInMonthResponse");
-        if (callback) callback(response.eresult, response);
-    }
-};
-handlers[Dota2.schema.lookupEnum("EDOTAGCMsg").values.k_EMsgGCLeaguesInMonthResponse] = onLeaguesInMonthResponse;
 
 var onLiveLeagueGameUpdate = function onLiveLeagueGameUpdate(message, callback) {
     callback = callback || null;
